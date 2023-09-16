b799cde0c694e8bf1424a97c4fd7da875e25732b
Mike Xiong <xiong.tchylon@gmail.com>
2023-09-16 16:20:01 +0200

    create pokemon card component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,27 @@
-// Composant Pokemon
-const PokemonImage = () => {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-            <figcaption>
-                Pokemon - Bulbizarre
-            </figcaption>
-        </figure>
-    );
-}
+// Style card
+const card = {
+  width: "200px",
+  padding: "1rem",
+  backgroundColor: "lightgreen",
+  borderRadius: "5px",
+  boxShadow: "0px 0px 5px gray",
+  color: "white",
+  fontSize: "1.6rem",
+  textAlign: "center",
+  textShadow: "0px 0px 5px gray",
+};
 
-export default PokemonImage;
+// Component card
+const PokemonCard = () => {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="bulbizarre"
+      />
+      <figcaption>Pokemon - Bulbizarre</figcaption>
+    </figure>
+  );
+};
+
+export default PokemonCard;
