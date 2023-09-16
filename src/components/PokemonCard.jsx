2249f90a553637b337b5ec7d526789f5e34dd375
Mike Xiong <xiong.tchylon@gmail.com>
2023-09-16 17:04:07 +0200

    update pokemon card component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,31 @@
-// Component card
+import React from "react";
+
+const pokemonList = [
+  {
+    name: "bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+// Composant de carte Pokemon
 const PokemonCard = () => {
   return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbizarre"
-      />
-      <figcaption>Pokemon - Bulbizarre</figcaption>
-    </figure>
+    <div>
+      {pokemonList.map((pokemon) => (
+        <figure>
+          {pokemon.imgSrc === undefined || pokemon.imgSrc === "" ? (
+            <p>???</p>
+          ) : (
+            <img src={pokemon.imgSrc} alt={`Photo de ${pokemon.name}`} />
+          )}
+          <figcaption>{`Pokémon : ${pokemon.name}`}</figcaption>
+        </figure>
+      ))}
+    </div>
   );
 };
 
