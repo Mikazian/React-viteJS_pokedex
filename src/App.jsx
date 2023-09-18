c064e08a25d859e19fb0dd6dc85383e824824744
Mike Xiong <xiong.tchylon@gmail.com>
2023-09-18 09:30:02 +0200

    add eventlistener for switch pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 import MyTitle from "./components/MyTitle";
 
@@ -8,12 +9,36 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "salamèche",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "carapuce",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
-  const pokemon = pokemonList[0];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const currentPokemon = pokemonList[pokemonIndex];
+
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const handleClickPrev = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
 
   return (
     <main>
@@ -24,8 +49,22 @@ function App() {
 
       {/* container pokemon card */}
       <div>
-        <PokemonCard pokemon={pokemon} />
+        <PokemonCard pokemon={currentPokemon} />
       </div>
+
+      {/* button previous for switch pokemon */}
+      {pokemonIndex > 0 ? (
+        <button onClick={handleClickPrev}>Précèdent</button>
+      ) : (
+        ""
+      )}
+
+      {/* button next for switch pokemon */}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleClickNext}>Suivant</button>
+      ) : (
+        ""
+      )}
     </main>
   );
 }
